237e479ad9251cb8d42998609e5ea3fe1c4a2021
Jinyan Li <jinyanli97@163.com>
2019-12-30 22:11:05 +0800

    2th

--- src/main/java/com/sufersay/demo/dao/PostDao.java
+++ src/main/java/com/sufersay/demo/dao/PostDao.java
@@ -30,9 +30,9 @@ public interface PostDao {
                         })
     List<postItem> getPostByUserId(int id);
 
-    @Select(value = "select posting._id,posting.post_time,user.username,posting.content,posting.comment_num,posting.collect_num,collection._id c_id" +
+    @Select(value = "select posting._id,posting.post_time,user.username,posting.content,posting.comment_num,posting.collect_num,GROUP_CONCAT(collection._id) c_id" +
             " from posting LEFT JOIN collection on posting._id = collection.posting_id" +
-            " LEFT JOIN user on posting.user_id = user._id ORDER BY posting.post_time DESC")
+            " LEFT JOIN user on posting.user_id = user._id GROUP BY posting._id ORDER BY posting.post_time DESC")
     @Results({
             @Result(column="_id",property="id"),
             @Result(column="post_time",property="postTime"),
@@ -44,9 +44,9 @@ public interface PostDao {
     List<postItem> getPostByTime();
 
     //按评论多少定义热度
-    @Select(value = "select posting._id,posting.post_time,user.username,posting.content,posting.comment_num,posting.collect_num,collection._id c_id" +
+    @Select(value = "select posting._id,posting.post_time,user.username,posting.content,posting.comment_num,posting.collect_num,GROUP_CONCAT(collection._id) c_id" +
             " from posting LEFT JOIN collection on posting._id = collection.posting_id" +
-            " LEFT JOIN user on posting.user_id = user._id ORDER BY comment_num DESC")
+            " LEFT JOIN user on posting.user_id = user._id GROUP BY posting._id ORDER BY comment_num DESC")
     @Results({
             @Result(column="_id",property="id"),
             @Result(column="post_time",property="postTime"),
@@ -58,9 +58,9 @@ public interface PostDao {
     List<postItem> getPostByHot();
 
     //按收藏查询
-    @Select(value = "select posting._id,posting.post_time,user.username,posting.content,posting.comment_num,posting.collect_num,collection._id c_id" +
+    @Select(value = "select posting._id,posting.post_time,user.username,posting.content,posting.comment_num,posting.collect_num,GROUP_CONCAT(collection._id) c_id" +
             " from posting LEFT JOIN collection on posting._id = collection.posting_id" +
-            " LEFT JOIN user on posting.user_id = user._id WHERE posting._id = collection.posting_id and collection.user_id=#{userId}")
+            " LEFT JOIN user on posting.user_id = user._id WHERE posting._id = collection.posting_id and collection.user_id=#{userId} GROUP BY posting._id")
     @Results({
             @Result(column="_id",property="id"),
             @Result(column="post_time",property="postTime"),
@@ -72,9 +72,9 @@ public interface PostDao {
     List<postItem> getPostByCollection(int userId);
 
     //按关键字查询
-    @Select(value = "select posting._id,posting.post_time,user.username,posting.content,posting.comment_num,posting.collect_num,collection._id c_id" +
+    @Select(value = "select posting._id,posting.post_time,user.username,posting.content,posting.comment_num,posting.collect_num,GROUP_CONCAT(collection._id) c_id" +
             " from posting LEFT JOIN collection on posting._id = collection.posting_id" +
-            " LEFT JOIN user on posting.user_id = user._id WHERE content LIKE '%${keyword}%'")
+            " LEFT JOIN user on posting.user_id = user._id WHERE content LIKE '%${keyword}%' GROUP BY posting._id")
     @Results({
             @Result(column="_id",property="id"),
             @Result(column="post_time",property="postTime"),
@@ -86,9 +86,9 @@ public interface PostDao {
     List<postItem> getPostByKeyword(@Param("keyword")String keyword);
 
     //按postId查询
-    @Select(value = "select posting._id,posting.post_time,user.username,posting.content,posting.comment_num,posting.collect_num,collection._id c_id" +
+    @Select(value = "select posting._id,posting.post_time,user.username,posting.content,posting.comment_num,posting.collect_num,GROUP_CONCAT(collection._id) c_id" +
             " from posting LEFT JOIN collection on posting._id = collection.posting_id" +
-            " LEFT JOIN user on posting.user_id = user._id WHERE posting._id =#{id}")
+            " LEFT JOIN user on posting.user_id = user._id WHERE posting._id =#{id} GROUP BY posting._id")
     @Results({
             @Result(column="_id",property="id"),
             @Result(column="post_time",property="postTime"),
